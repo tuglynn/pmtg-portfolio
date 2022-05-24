7961341877683f296fb42a7328d73992158fc4c2
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-05-24 11:56:01 -0700

    changed project links to deployed app instead of github repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,18 @@ const project3 = document.querySelector('.project3');
 mainProject.addEventListener('click', () => {
     mainProject.innerHTML =
         `<h2>an online virtual pet app
-     <a href="https://github.com/monacoglynn/gp-mini-pet">Click to see more</a></h2>`
+     <a href="https://monacoglynn.github.io/gp-mini-pet/">Click to see more</a></h2>`
 });
 project2.addEventListener('click', () => {
     project2.innerHTML =
         `<h2>weather tracking app that saves your searched cities
-    <a href="https://github.com/monacoglynn/weather-tracker">Click to see more</a></h2>`
+    <a href="https://monacoglynn.github.io/weather-tracker/">Click to see more</a></h2>`
 })
 
 project3.addEventListener('click', () => {
     project3.innerHTML =
         `<h2>challenge your knowledge with this quiz game
-    <a href="https://github.com/monacoglynn/piano-quiz">Click to see more</a></h2>`
+    <a href="https://monacoglynn.github.io/piano-quiz/index.html">Click to see more</a></h2>`
 })
 
 
