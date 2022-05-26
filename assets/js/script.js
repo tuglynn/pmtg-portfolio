aac16623cbb388388e611b246e4d67bd759e2cad
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-05-26 13:30:29 -0700

    fixed the click function on projects.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,33 @@ const mainProject = document.querySelector('.main-project');
 const project2 = document.querySelector('.project2');
 const project3 = document.querySelector('.project3');
 
+mainProject.addEventListener('click', () => location.href = 'https://monacoglynn.github.io/gp-mini-pet/')
 
 mainProject.addEventListener('mouseover', () => {
     mainProject.innerHTML =
-        `<h2>an online virtual pet app
-     <a href="https://monacoglynn.github.io/gp-mini-pet/">Click to see more</a></h2>`
+        `<h2>an online virtual pet app. Click to see more</h2>`
 });
 mainProject.addEventListener('mouseout', () => {
     mainProject.innerHTML = `<h2>burauza no akachan</h2>`
-})
+});
+
+project2.addEventListener('click', () => location.href = 'https://monacoglynn.github.io/weather-tracker/')
+
 project2.addEventListener('mouseover', () => {
     project2.innerHTML =
-        `<h2>weather tracking app that saves your searched cities
-    <a href="https://monacoglynn.github.io/weather-tracker/">Click to see more</a></h2>`
-})
+        `<h2>weather tracking app that saves your searched cities. Click to see more</h2>`
+});
 project2.addEventListener('mouseout', () => {
     project2.innerHTML = `<h2>weather app</h2>`
-})
+});
+
+project3.addEventListener('click', () => location.href = 'https://monacoglynn.github.io/piano-quiz/index.html')
 
 project3.addEventListener('mouseover', () => {
     project3.innerHTML =
-        `<h2>challenge your knowledge with this quiz game
-    <a href="https://monacoglynn.github.io/piano-quiz/index.html">Click to see more</a></h2>`
-})
+        `<h2>challenge your knowledge with this quiz game. Click to see more</h2>`
+});
 
 project3.addEventListener('mouseout', () => {
     project3.innerHTML = `<h2>piano quiz</h2>`
-})
-
-//when you click on the picture of the project, a brief description is displayed and a link to the page is displayed as well.
+});
